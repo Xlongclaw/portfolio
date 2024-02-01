f0e633d756b3a3ef37c6e12e6e4a7c6946e6b9ce
unknown <xlongclaw@gmail.com>
2024-02-01 18:05:21 +0530

    redesigned Option panel UI

--- src/components/OptionPanel.jsx
+++ src/components/OptionPanel.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 
 const OptionPanel = () => {
-  const [selected,setSelected] = useState(0)
+  const [selected,setSelected] = useState(1)
   return (
     <section className=" w-1/5 flex flex-col space-y-1 overflow-hidden text-sm font-semibold">
-      <div onClick={()=>setSelected(0)} className={` ${selected===0?"text-ASCENT border-[px]":""} bg-DARK-III w-full h-1/3 flex justify-center items-center border-0 border-ASCENT hover:bg-ASCENT hover:border-[1px] duration-100 transition-all hover:cursor-pointer hover:text-black`}>
-       ABOUT ME
-      </div>
-      <div onClick={()=>setSelected(1)} className={` ${selected===1?"text-ASCENT ":""} bg-DARK-III w-full h-1/3 flex justify-center items-center border-0 border-ASCENT hover:bg-ASCENT hover:border-[1px] duration-100 transition-all hover:cursor-pointer hover:text-black`}>
+
+      <div onClick={()=>setSelected(0)} className={` ${selected===0?"text-black bg-ASCENT ":""} bg-DARK-III  w-full h-1/3 flex justify-center items-center border-0 border-ASCENT hover:bg-ASCENT hover:border-[1px] duration-100 transition-all hover:cursor-pointer hover:text-black`}>
         MY PROJECTS
       </div>
-      <div onClick={()=>setSelected(2)} className={` ${selected===2?"text-ASCENT ":""} bg-DARK-III w-full h-1/3 flex justify-center items-center border-0 border-ASCENT hover:bg-ASCENT hover:border-[1px] duration-100 transition-all hover:cursor-pointer hover:text-black`}>
+      <div onClick={()=>setSelected(1)} className={` ${selected===1?"text-black bg-ASCENT ":""} bg-DARK-III  w-full h-1/3 flex justify-center items-center border-0 border-ASCENT hover:bg-ASCENT hover:border-[1px] duration-100 transition-all hover:cursor-pointer hover:text-black`}>
+        ABOUT ME
+      </div>
+      <div onClick={()=>setSelected(2)} className={` ${selected===2?"text-black bg-ASCENT ":""} bg-DARK-III  w-full h-1/3 flex justify-center items-center border-0 border-ASCENT hover:bg-ASCENT hover:border-[1px] duration-100 transition-all hover:cursor-pointer hover:text-black`}>
         MY HARDCORE SKILLS
       </div>
+      
     </section>
   );
 };
