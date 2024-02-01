403e5dc8c3e5d29d8e64c4cc0b54c441331c7744
unknown <xlongclaw@gmail.com>
2024-02-01 17:39:08 +0530

    Designed OptionPanel UI

--- src/components/OptionPanel.jsx
+++ src/components/OptionPanel.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const OptionPanel = () => {
+  const [selected,setSelected] = useState(0)
   return (
-    <section className='bg-DARK-III w-1/5'></section>
-  )
-}
+    <section className=" w-1/5 flex flex-col space-y-1 overflow-hidden text-sm font-semibold">
+      <div onClick={()=>setSelected(0)} className={` ${selected===0?"text-ASCENT border-b-[1px] ":""} bg-DARK-III w-full h-1/3 flex justify-center items-center border-0 border-ASCENT hover:border-y-[4px] duration-100 transition-all hover:cursor-pointer hover:text-ASCENT`}>
+        ALL ABOUT ME
+      </div>
+      <div onClick={()=>setSelected(1)} className={` ${selected===1?"text-ASCENT border-b-[1px] ":""} bg-DARK-III w-full h-1/3 flex justify-center items-center border-0 border-ASCENT hover:border-y-[4px] duration-100 transition-all hover:cursor-pointer hover:text-ASCENT`}>
+        MY PROJECTS
+      </div>
+      <div onClick={()=>setSelected(2)} className={` ${selected===2?"text-ASCENT border-b-[1px] ":""} bg-DARK-III w-full h-1/3 flex justify-center items-center border-0 border-ASCENT hover:border-y-[4px] duration-100 transition-all hover:cursor-pointer hover:text-ASCENT`}>
+        MY HARDCORE SKILLS
+      </div>
+    
+    </section>
+  );
+};
 
-export default OptionPanel
+export default OptionPanel;
