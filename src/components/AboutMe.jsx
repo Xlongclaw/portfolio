cc2bb399d24499e333bd1210c8c4e21a8e9e6cd8
unknown <xlongclaw@gmail.com>
2024-02-02 11:16:16 +0530

    Added animation to profile image

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -11,31 +11,34 @@ const AboutMe = () => {
       animate={{ opacity: [0, 1], x: [100, 0] }}
       transition={{ duration: 0.5 }}
     >
-      <div className="relative">
+      <motion.div whileHover={{scale:1.3,x:30}} transition={{type:"spring",stiffness:80}} className="relative w-48">
         <img
           className="pb-8 w-48 relative z-10
         "
           src={require("../images/Profile-Image.png")}
           alt=""
         />
-        <div className="absolute flex flex-col gap-2 top-0 left-10 rotate-45">
-        <div className="h-[4px] bg-ASCENT/70 w-48 rounded-full"></div>
-        <div className="h-[4px] bg-ASCENT/70 w-48 rounded-full"></div>
-        <div className="h-[4px] bg-ASCENT/70 w-48 rounded-full"></div>
-        <div className="h-[4px] bg-ASCENT/70 w-48 rounded-full"></div>
-        <div className="h-[4px] bg-ASCENT/70 w-48 rounded-full"></div>
-        <div className="h-[4px] bg-ASCENT/70 w-48 rounded-full"></div>
-        <div className="h-[4px] bg-ASCENT/70 w-48 rounded-full"></div>
-        <div className="h-[4px] bg-ASCENT/70 w-48 rounded-full"></div>
-        <div className="h-[4px] bg-ASCENT/70 w-48 rounded-full"></div>
-        <div className="h-[4px] bg-ASCENT/70 w-48 rounded-full"></div>
-        <div className="h-[4px] bg-ASCENT/70 w-48 rounded-full"></div>
-        <div className="h-[4px] bg-ASCENT/70 w-48 rounded-full"></div>
-        <div className="h-[4px] bg-ASCENT/70 w-48 rounded-full"></div>
-        <div className="h-[4px] bg-ASCENT/70 w-48 rounded-full"></div>
+        <motion.div className="absolute flex flex-col w-48 gap-2 top-0 left-10 rotate-45">
+          
+          <div className="h-[2px] bg-ASCENT/70 w-full"></div>
+          <div className="h-[2px] bg-ASCENT/70 w-full"></div>
+          <div className="h-[2px] bg-ASCENT/70 w-full"></div>
+          <div className="h-[2px] bg-ASCENT/70 w-full"></div>
+          <div className="h-[2px] bg-ASCENT/70 w-full"></div>
+          <div className="h-[2px] bg-ASCENT/70 w-full"></div>
+          <div className="h-[2px] bg-ASCENT/70 w-full"></div>
+          <div className="h-[2px] bg-ASCENT/70 w-full"></div>
+          <div className="h-[2px] bg-ASCENT/70 w-full"></div>
+          <div className="h-[2px] bg-ASCENT/70 w-full"></div>
+          <div className="h-[2px] bg-ASCENT/70 w-full"></div>
+          <div className="h-[2px] bg-ASCENT/70 w-full"></div>
+          <div className="h-[2px] bg-ASCENT/70 w-full"></div>
+          <div className="h-[2px] bg-ASCENT/70 w-full"></div>
+          <div className="h-[2px] bg-ASCENT/70 w-full"></div>
 
-        </div>
-      </div>
+
+        </motion.div>
+      </motion.div>
       <HeroText />
       <SocialLinks />
       <div className="absolute right-0 top-0 hover:text-black cursor-pointer hover:bg-ASCENT p-4 transition-all rounded-br-3xl flex items-center gap-3">
--- src/components/OptionPanel.jsx
+++ src/components/OptionPanel.jsx
@@ -1,17 +1,30 @@
-const OptionPanel = ({setSelectedOption,selectedOption}) => {
+const OptionPanel = ({ setSelectedOption, selectedOption }) => {
   return (
     <section className=" w-1/5 flex flex-col space-y-1 overflow-hidden text-sm font-semibold">
-
-      <div onClick={()=>setSelectedOption(0)} className={` ${selectedOption===0?"text-black bg-ASCENT ":"bg-DARK-III"}   w-full h-1/3 flex justify-center items-center border-0 border-ASCENT hover:bg-ASCENT hover:border-[1px] duration-100 transition-all hover:cursor-pointer hover:text-black`}>
+      <div
+        onClick={() => setSelectedOption(0)}
+        className={` ${
+          selectedOption === 0 ? "text-black bg-ASCENT " : "bg-DARK-III"
+        }   w-full h-1/3 flex justify-center items-center border-0 border-ASCENT hover:bg-ASCENT hover:border-[1px] duration-100 transition-all hover:cursor-pointer hover:text-black`}
+      >
         MY PROJECTS
       </div>
-      <div onClick={()=>setSelectedOption(1)} className={` ${selectedOption===1?"text-black bg-ASCENT ":"bg-DARK-III"}   w-full h-1/3 flex justify-center items-center border-0 border-ASCENT hover:bg-ASCENT hover:border-[1px] duration-100 transition-all hover:cursor-pointer hover:text-black`}>
+      <div
+        onClick={() => setSelectedOption(1)}
+        className={` ${
+          selectedOption === 1 ? "text-black bg-ASCENT " : "bg-DARK-III"
+        }   w-full h-1/3 flex justify-center items-center border-0 border-ASCENT hover:bg-ASCENT hover:border-[1px] duration-100 transition-all hover:cursor-pointer hover:text-black`}
+      >
         ABOUT ME
       </div>
-      <div onClick={()=>setSelectedOption(2)} className={` ${selectedOption===2?"text-black bg-ASCENT ":"bg-DARK-III"}   w-full h-1/3 flex justify-center items-center border-0 border-ASCENT hover:bg-ASCENT hover:border-[1px] duration-100 transition-all hover:cursor-pointer hover:text-black`}>
+      <div
+        onClick={() => setSelectedOption(2)}
+        className={` ${
+          selectedOption === 2 ? "text-black bg-ASCENT " : "bg-DARK-III"
+        }   w-full h-1/3 flex justify-center items-center border-0 border-ASCENT hover:bg-ASCENT hover:border-[1px] duration-100 transition-all hover:cursor-pointer hover:text-black`}
+      >
         MY HARDCORE SKILLS
       </div>
-      
     </section>
   );
 };
