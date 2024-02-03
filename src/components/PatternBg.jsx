5d18638fd083b764e7d0dd4a47e77ebe0ecca95c
unknown <xlongclaw@gmail.com>
2024-02-03 13:42:25 +0530

    Animated patternBg

--- src/components/PatternBg.jsx
+++ src/components/PatternBg.jsx
@@ -1,63 +1,233 @@
-import React from 'react'
+import React from "react";
 import { motion } from "framer-motion";
 
-
 const PatternBg = () => {
+  const anim = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: { staggerChildren: 0.06, delayChildren: 0.04 },
+    },
+  };
+  const child = {
+    hidden: { opacity: 0 },
+    visible: (i = 1) => ({
+      opacity: 1,
+      x:[-1000,0],
+      transition: { repeat: Infinity, duration: 2 },
+    }),
+  };
+
   return (
-    <motion.div className="absolute flex flex-col w-48 gap-2 bottom-[0rem] right-0 -rotate-45 ">
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
+    <motion.div
+      variants={anim}
+      initial="hidden"
+      animate="visible"
+      className="absolute flex flex-col w-48 gap-2 bottom-[0rem] right-0 -rotate-45 "
+    >
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
 
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[100rem]"></div>
-      <div className="h-[2px] bg-DARK-I/100 w-[s100rem]"></div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="h-[2px] bg-DARK-I/100 w-[100rem]"
+      ></motion.div>
     </motion.div>
-  )
-}
+  );
+};
 
-export default PatternBg
+export default PatternBg;
