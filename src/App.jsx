731c325285697ef1e0f486e8cb546e83b73716ae
unknown <xlongclaw@gmail.com>
2024-02-12 21:08:41 +0530

    increase pb media query <xs  in aboutMe

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { MainPanel, OptionPanel } from "./components";
 const App = () => {
   const [selectedOption, setSelectedOption] = useState(1);
   return (
-    <div className="bg-DARK-III text-WHITE sm:p-4 p-2 h-screen font-inter">
+    <div className="bg-DARK-III text-WHITE xs:pt-2 xs:pb-2 xs:px-2 pt-2 pb-8 px-2 h-screen font-inter">
       <div className="p-1 bg-DARK-I h-full">
         <main className="flex sm:gap-1 gap-[0.20rem] h-full">
           <MainPanel selectedOption={selectedOption} />
--- src/components/MainPanel.jsx
+++ src/components/MainPanel.jsx
@@ -5,7 +5,7 @@ import Skills from "./Skills";
 
 const MainPanel = ({ selectedOption }) => {
   return (
-    <section className="bg-DARK-III sm:w-4/5 w-[88%] xl:p-16 lg:px-12 lg:py-20 md:px-10 md:py-24 sm:px-8 sm:py-24 px-6 py-24 overflow-hidden">
+    <section className="bg-DARK-III sm:w-4/5 w-[88%] xl:p-16 lg:px-12 lg:py-20 md:px-10 md:py-24 sm:px-8 sm:py-20 px-6 py-20 overflow-hidden">
       {selectedOption === 0 ? (
         <MyProjects />
       ) : selectedOption === 1 ? (
