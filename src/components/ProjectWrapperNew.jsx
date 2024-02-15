e93e86ac81e846a1c0ea50c50adaa3d6c87f09bb
unknown <xlongclaw@gmail.com>
2024-02-15 19:48:11 +0530

    added responsiveness to my projects section

--- src/components/ProjectWrapperNew.jsx
+++ src/components/ProjectWrapperNew.jsx
@@ -5,15 +5,15 @@ const ProjectWrapperNew = ({ project }) => {
   return (
     <div className="w-[100%] sm:w-[24rem] flex flex-col gap-2">
       <img
-        className="w-full sm:h-3/4 h-[50vw] object-cover object-left-top border border-WHITE/10"
+        className="w-full sm:h-[15rem] h-[50vw] object-cover object-left-top border border-WHITE/10"
         src={project.image}
         alt=""
       />
       <div className="w-full flex sm:flex-row flex-col items-center sm:gap-1 gap-2">
-        <h3 className="border border-WHITE/10 sm:w-[13rem] w-full sm:flex-1 sm:h-[3.5rem] h-[3rem] flex justify-center items-center text-xs">
+        <h3 className="border border-WHITE/10 sm:w-[18rem] w-full sm:flex-1 sm:h-[3.5rem] h-[3rem] flex justify-center items-center text-xs">
           {project.name.toUpperCase()}
         </h3>
-        <div className="flex sm:h-[3.5rem] h-[3rem] w-full gap-1">
+        <div className="flex sm:h-[3.5rem] h-[3rem] w-full sm:w-auto gap-1">
           <BsGithub className="border border-WHITE/10 flex sm:w-[3.5rem] w-1/3 h-full sm:p-5 p-4" />
           <BsDatabase className="border border-WHITE/10 flex sm:w-[3.5rem] w-1/3 h-full sm:p-5 p-4" />
           <BsChevronRight className="border border-WHITE/10 flex sm:w-[3.5rem] w-1/3 h-full p-4 sm:p-5" />
