f4982e514408405fbee23c60adf9fed06a121749
unknown <xlongclaw@gmail.com>
2024-02-16 20:09:39 +0530

    removed excessive code and organized MyProjects

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -17,8 +17,10 @@ const MyProjects = () => {
       variants={parentVarient}
       initial="hidden"
       animate="visible"
-      className="grid relative sm:top-0 top-[-3.5rem] w-full sm:h-full h-[116%] sm:gap-10 gap-10 lg:grid-flow-col lg:overflow-x-scroll lg:overflow-y-hidden
-       overflow-y-scroll overflow-x-hidden md:grid-flow-row sm:pr-0 pr-1 hide-scroll pb-4"
+      className="grid relative gap-10 overflow-y-scroll overflow-x-hidden top-[-3.5rem] w-full h-[116%] pr-1 pb-4 hide-scroll
+      sm:top-0 sm:h-full sm:gap-10 sm:pr-0
+      md:grid-flow-row
+      lg:grid-flow-col lg:overflow-x-scroll lg:overflow-y-hidden"
     >
       <div className="flex flex-wrap gap-6 w-full overflow-y-scroll justify-center hide-scroll overflow-hidden">
         {projectsData.map((project, i) => (
